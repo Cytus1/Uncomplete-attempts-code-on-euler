ff5045ab7cd9c900a7fb207c97c3711bdcef9e2d
Weiyang Zhang <109835252+Cytus1@users.noreply.github.com>
2022-08-05 08:47:27 -0400

    Update projecteuler.net question 7 attempt.py
    this code went completely wrong

--- projecteuler.net question 7 attempt.py
+++ projecteuler.net question 7 attempt.py
@@ -7,16 +7,15 @@ not_prime_list = []
 def find_prime(x):
     if x == 2:
         prime_list.append(x)
-        
+
     range_for_divisor = math.floor(math.sqrt(x))
     for check_factor in range(3, range_for_divisor + 1):
         if x % check_factor == 0:
             not_prime_list.append(check_factor)
-    prime_list.append(check_factor)
+        if x % check_factor != 0:
+            prime_list.append(check_factor)
 
 
 find_prime(1000)
 print(not_prime_list)
 print(prime_list)
-
-#   idea : set range 10001, list.sort(), print ("name", list[-1])
