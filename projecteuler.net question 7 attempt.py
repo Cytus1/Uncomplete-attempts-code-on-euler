4aea3c13ff5becab7fc36a1edcc4e77128cd0b45
Weiyang Zhang <109835252+Cytus1@users.noreply.github.com>
2022-08-23 10:33:25 -0400

    Update projecteuler.net question 7 attempt.py

--- projecteuler.net question 7 attempt.py
+++ projecteuler.net question 7 attempt.py
@@ -2,22 +2,26 @@
 import math
 prime_number_count = 0
 #   function to define prime
+
+
+def find_factor(value):  # find factor
+    factors = []
+    for check_factor in range(1, int(math.sqrt(value)) + 1):
+          if value % check_factor == 0:
+              factors.append(check_factor)
+              factors.append(value // check_factor)
+          return factors
+
+
+def is_prime(value):
+    return len(find_factor(value)) == 2
+
+
+factors_find = find_factor(prime_number_count)
+largest_prime_factor = 0
+for factor in factors_find:
+    if is_prime(factor) and factor > largest_prime_factor:
+        largest_prime_factor = factor
+
 while prime_number_count < 10001:
-    def find_factor(value):  # find factor
-        factors = []
-        for check_factor in range(1, int(math.sqrt(value)) + 1):
-            if value % check_factor == 0:
-                factors.append(check_factor)
-                factors.append(value // check_factor)
-        return factors
-
-
-    def is_prime(value):
-        return len(find_factor(value)) == 2
-
-
-    factors_find = find_factor(prime_number_count)
-    largest_prime_factor = 0
-    for factor in factors_find:
-        if is_prime(factor) and factor > largest_prime_factor:
-            largest_prime_factor = factor
+    
